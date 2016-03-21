40b3d5281437294f1df40f0b5a7eb09ca864075d
Oliver Dozsa <salaj1986@gmail.com>
2016-03-21 20:54:23 +0100

    Some testing updates.

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -17,7 +17,7 @@ addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.4.2")
 addSbtPlugin("com.typesafe.sbt" % "sbt-play-enhancer" % "1.1.0")
 
 // Play Ebean support, to enable, uncomment this line, and enable in your build.sbt using
-addSbtPlugin("com.typesafe.sbt" % "sbt-play-ebean" % "1.0.0")
+addSbtPlugin("com.typesafe.sbt" % "sbt-play-ebean" % "3.0.0")
 
 // Eclipse support
 addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "4.0.0")
--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,38 +1,44 @@
 import org.junit.*;
 
+import models.Ingredient;
+import models.IngredientName;
 import models.Language;
 import play.db.Database;
 import play.db.Databases;
 import play.db.evolutions.Evolutions;
 
 import static play.test.Helpers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class IntegrationTest
 {
-    private Database database;
+    private Map<String, Object> config;
     
     @Before
     public void setup()
     {
-        //database = Databases.inMemory();
+        config = new HashMap<String, Object>();
+        
+        config.putAll(inMemoryDatabase("default"));
+        config.put("play.evolutions.db.default.autoApply", "true");
     }
     
     @After
     public void tearDown()
     {
-        //database.shutdown();
+        
     }
     
     @Test
     public void testLangauge()
-    {
-        //Evolutions.applyEvolutions(database);
-        
-        
+    {   
         running
         (
-            fakeApplication(),
+            fakeApplication(config),
             new Runnable()
             {
                 public void run()
@@ -50,12 +56,57 @@ public class IntegrationTest
                     Language dbTestLang = Language.find.where().eq("isoName", testIsoName).findUnique();
                     
                     assertEquals("DB name and test name are not the same!", testIsoName, dbTestLang.isoName);
+                    
+                    /* Modify */
+                    testIsoName = "new test iso name";
+                    
+                    dbTestLang.isoName = testIsoName;
+                    
+                    dbTestLang.save();
+                    
+                    dbTestLang = Language.find.where().eq("isoName", testIsoName).findUnique();
+                    
+                    assertEquals("DB name and test name are not the same after modification!", testIsoName, dbTestLang.isoName);
+                    
+                    /* Delete. */
+                    
+                    assertTrue("Could not delete language!", dbTestLang.delete());
                 }
             }
         );
-        
-        
-        //Evolutions.cleanupEvolutions(database);
     }
-
+    
+    @Test
+    public void testIngredient()
+    {
+        running
+        (
+            fakeApplication(config),
+            new Runnable()
+            {
+                public void run()
+                {
+                    /* Create. */
+                    String testIngNameStr = "testIngName";
+                    
+                    Ingredient testIngredient = new Ingredient();
+                    
+                    testIngredient.save();
+                    
+                    IngredientName testIngName = new IngredientName();
+                    
+                    testIngName.ingredient = testIngredient;
+                    testIngName.name       = testIngNameStr;
+                    
+                    // TODO
+                    
+                    
+                    /* Modify. */
+                    
+                    
+                    /* Delete. */
+                }
+            }
+        );
+    }
 }
